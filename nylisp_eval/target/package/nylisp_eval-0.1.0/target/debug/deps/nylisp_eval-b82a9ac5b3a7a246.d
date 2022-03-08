02dc0638d2048ad310bdfb9a6dd7e7fb51dea21c
jumang4423 <catontheskate4423@gmail.com>
2022-03-08 17:45:39 +0900

    feature: added test lib

--- nylisp_eval/target/package/nylisp_eval-0.1.0/target/debug/deps/nylisp_eval-b82a9ac5b3a7a246.d
+++
@@ -1,7 +0,0 @@
-/Users/jumang4423/PROJECTS/rust/nylisp/nylisp_eval/target/package/nylisp_eval-0.1.0/target/debug/deps/nylisp_eval-b82a9ac5b3a7a246.rmeta: src/lib.rs
-
-/Users/jumang4423/PROJECTS/rust/nylisp/nylisp_eval/target/package/nylisp_eval-0.1.0/target/debug/deps/libnylisp_eval-b82a9ac5b3a7a246.rlib: src/lib.rs
-
-/Users/jumang4423/PROJECTS/rust/nylisp/nylisp_eval/target/package/nylisp_eval-0.1.0/target/debug/deps/nylisp_eval-b82a9ac5b3a7a246.d: src/lib.rs
-
-src/lib.rs:
